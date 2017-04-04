842204a85b20a8f1cab6183c0f9a63a8bf7c3d10
Andreas Lindhé <andreas@lindhe.io>
2017-04-04 21:08:13 +0200

    Update Sway env variables

--- .config/sway/sway-environment.sh
+++ .config/sway/sway-environment.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # Make /usr/share/wayland-sessions/sway.desktop run this file.
-export XKB_DEFAULT_LAYOUT=se-A5; sway
+export XKB_DEFAULT_LAYOUT=se-A5;
+export XKB_REPEAT_DELAY=75;
+export XKB_REPEAT_RATE=330;
+export GDK_BACKEND=wayland;
+export CLUTTER_BACKEND=wayland;
+sway
