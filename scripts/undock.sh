1e0c8a84b4210f6117b6ee1c21f31ccfb94da82c
Andreas Lindhé <andreas@lindhe.io>
2017-07-16 13:08:17 +0200

    Source .xprofile in undock script

--- scripts/undock.sh
+++ scripts/undock.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-xrandr --output eDP-1 --auto --primary --output HDMI-1 --off --output DP-1 --off --output DP-2 --off && echo 'Monitors off'
+xrandr --output eDP-1 --auto --primary \
+    --output HDMI-1 --off --output DP-1 --off --output DP-2 --off \
+    && echo 'Monitors off';
+~/.xprofile
