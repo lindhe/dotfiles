754b13631d369ad0fb25f3cb0c28f0e5f0079b12
Andreas Lindhé <andreas@lindhe.io>
2017-11-02 18:17:23 +0100

    Add logging to docking script

--- scripts/dock/toggle_dock.sh
+++ scripts/dock/toggle_dock.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+RES=''
+
 # If the dock ethernet interface is available, we are probably docked.
 ip link show eth0 2> /dev/null > /dev/null;
 
 if [ $? -eq 0 ]; then
     ~/scripts/dock/monman.py -a \
-        && echo 'Docking successful!' || echo 'Docking failed!'
+        && $RES='Docking successful!' || $RES='Docking failed!';
 else
     ~/scripts/dock/monman.py -d \
-        && echo 'Undocked!' || echo 'Failed to undock!'
+        && $RES='Undocking sucessful!' || $RES='Undocking failed!';
 fi
+
+logger $RES;
 
 # Resource .xprofile
 ~/.xprofile
