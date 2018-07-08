c8020a85bb870d7cb43842093b207c463c6f7ea5
Andreas Lindhé <andreas@lindhe.io>
2018-07-08 19:43:18 +0200

    Fix typo in toggle_dock.sh

--- scripts/dock/toggle_dock.sh
+++ scripts/dock/toggle_dock.sh
@@ -6,10 +6,10 @@ RES=''
 
 if [ $? -eq 0 ]; then
     ~/scripts/dock/monman.py -a \
-        && $RES='Docking successful!' || $RES='Docking failed!';
+        && RES='Docking successful!' || RES='Docking failed!';
 else
     ~/scripts/dock/monman.py -d \
-        && $RES='Undocking sucessful!' || $RES='Undocking failed!';
+        && RES='Undocking sucessful!' || RES='Undocking failed!';
 fi
 
 logger $RES;
