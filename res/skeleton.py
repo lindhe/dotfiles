d36b91a320c6a59b27c4ada8424c73d671b15012
Andreas Lindhé <andreas@lindhe.io>
2020-07-18 18:39:10 +0200

    Make skeleton.py PEP8 compliant

--- res/skeleton.py
+++ res/skeleton.py
@@ -11,18 +11,20 @@ This is where general documentation should go.
 import sys
 import argparse
 
+
 def main():
-  """ Main function description """
+    """ Main function description """
+    pass
+
 
 if __name__ == '__main__':
-  # Bootstrapping
-  p = argparse.ArgumentParser(description="Skeleton file for Python!")
-  # Add cli arguments
-  # p.add_argument('-f', '--foo', help="Just a dummmy flag")
-  # Run:
-  args = p.parse_args()
-  try:
-    main()
-  except KeyboardInterrupt:
-    sys.exit("\nInterrupted by ^C\n")
-
+    # Bootstrapping
+    p = argparse.ArgumentParser(description="Skeleton file for Python!")
+    # Add cli arguments
+    # p.add_argument('-f', '--foo', help="Just a dummmy flag")
+    # Run:
+    args = p.parse_args()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit("\nInterrupted by ^C\n")
