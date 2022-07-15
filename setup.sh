bb05f6366a54ea9622b44fbe7c6a7382dd446381
Andreas Lindhé <andreas@lindhe.io>
2022-07-15 15:59:29 +0200

    [setup] Sort symlinks

--- setup.sh
+++ setup.sh
@@ -14,14 +14,14 @@ echo "Creating ~/.undodir for vim"
 mkdir -p ~/.undodir
 
 echo "Creating symlinks …"
-ln -s ./git/lindhe/dotfiles/.vimrc .
-ln -s ./git/lindhe/dotfiles/.vim .
-ln -s ./git/lindhe/dotfiles/.zshrc .
-ln -s ./git/lindhe/dotfiles/.xprofile .
-ln -s ./git/lindhe/dotfiles/.tmux.conf .
-ln -s ./git/lindhe/dotfiles/res .
-ln -s ./git/lindhe/dotfiles/.gitignore_global .
 ln -s ./git/lindhe/dotfiles/.gitconfig .
+ln -s ./git/lindhe/dotfiles/.gitignore_global .
+ln -s ./git/lindhe/dotfiles/.tmux.conf .
+ln -s ./git/lindhe/dotfiles/.vim .
+ln -s ./git/lindhe/dotfiles/.vimrc .
+ln -s ./git/lindhe/dotfiles/.xprofile .
+ln -s ./git/lindhe/dotfiles/.zshrc .
+ln -s ./git/lindhe/dotfiles/res .
 
 mkdir -p ~/.config
 cd ~/.config/
@@ -30,8 +30,8 @@ ln -s ../git/lindhe/dotfiles/.config/compton.conf .
 ln -s ../git/lindhe/dotfiles/.config/dunst/ .
 ln -s ../git/lindhe/dotfiles/.config/i3 .
 ln -s ../git/lindhe/dotfiles/.config/i3blocks/ .
-ln -s ../git/lindhe/dotfiles/.config/nvim/ .
 ln -s ../git/lindhe/dotfiles/.config/monitors/ .
+ln -s ../git/lindhe/dotfiles/.config/nvim/ .
 ln -s ../git/lindhe/dotfiles/.config/sway .
 ln -s ../git/lindhe/dotfiles/.config/zathura/ .
 ln -s ../git/lindhe/dotfiles/.config/zsh/ .
