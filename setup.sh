e6e959b54631e289e672b0db72332a7ec6531812
Andreas Lindhé <andreas@lindhe.io>
2022-07-15 15:28:09 +0200

    [setup] Minor refactoring

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ pushd ~
 echo "Creating ~/.undodir for vim"
 mkdir -p ~/.undodir
 
-echo "Creating symlinks in ~/"
+echo "Creating symlinks …"
 ln -s ~/git/lindhe/dotfiles/.vimrc .
 ln -s ~/git/lindhe/dotfiles/.vim .
 ln -s ~/git/lindhe/dotfiles/.zshrc .
@@ -23,9 +23,9 @@ ln -s ~/git/lindhe/dotfiles/res .
 ln -s ~/git/lindhe/dotfiles/.gitignore_global .
 ln -s ~/git/lindhe/dotfiles/.gitconfig .
 
-echo "Creating symlinks in ~/.config/"
 mkdir -p ~/.config
-pushd ~/.config/
+cd ~/.config/
+
 ln -s ~/git/lindhe/dotfiles/.config/compton.conf .
 ln -s ~/git/lindhe/dotfiles/.config/dunst/ .
 ln -s ~/git/lindhe/dotfiles/.config/i3 .
@@ -35,7 +35,6 @@ ln -s ~/git/lindhe/dotfiles/.config/monitors/ .
 ln -s ~/git/lindhe/dotfiles/.config/sway .
 ln -s ~/git/lindhe/dotfiles/.config/zathura/ .
 ln -s ~/git/lindhe/dotfiles/.config/zsh/ .
-popd
 
 echo "Installing custom keyboard layouts"
 sudo cp ~/git/lindhe/dotfiles/usr/share/X11/xkb/symbols/* -t /usr/share/X11/xkb/symbols
