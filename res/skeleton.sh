784038ce99867111df464783921a5c097d721ba6
Andreas Lindhé <andreas@lindhe.io>
2022-08-03 00:31:57 +0200

    [res] Add common functions to skeleton.sh

--- res/skeleton.sh
+++ res/skeleton.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
+debug() {
+    echo "${@}" 1>&2
+}
+
+fail() {
+    debug "${1}"
+    exit "${2:-1}"
+}
+
+if [[ $# -ne 2 ]]; then
+    debug ""
+    debug "USAGE:"
+    debug "    ${0} SSID PASSWORD"
+    debug ""
+    exit 0
+fi
