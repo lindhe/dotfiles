096611c2b9ba91a5bf0707b45ea75167e61a3b03
Andreas Lindhé <7773090+lindhe@users.noreply.github.com>
2025-01-06 13:33:42 +0100

    nvim,res: Remove LaTeX config

--- res/latex/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-MAINFILE=main
-pdflatex -shell-escape -haltonerror $MAINFILE.tex &&\
-bibtex8 $MAINFILE.aux;\
-pdflatex -shell-escape -haltonerror $MAINFILE.tex &&\
-pdflatex -shell-escape -haltonerror $MAINFILE.tex
