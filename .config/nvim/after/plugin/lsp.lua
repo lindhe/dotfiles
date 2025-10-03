a0fce563b7954dbe4d0298170602a5d020b0d2ff
Andreas Lindhé <7773090+lindhe@users.noreply.github.com>
2025-10-03 10:15:00 +0200

    nvim: Indent lsp.lua

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,31 +1,32 @@
 if not vim.g.vscode then
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '§', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', '½', vim.diagnostic.goto_next, opts)
+  -- Mappings.
+  -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+  local opts = { noremap=true, silent=true }
+  vim.keymap.set('n', '§', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', '½', vim.diagnostic.goto_next, opts)
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-end
+  -- Use an on_attach function to only map the following keys
+  -- after the language server attaches to the current buffer
+  local on_attach = function(client, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  end
 
-require('lspconfig')['bashls'].setup {
+  require('lspconfig')['bashls'].setup {
     on_attach = on_attach,
-}
-require('lspconfig')['rust_analyzer'].setup {
+  }
+  require('lspconfig')['rust_analyzer'].setup {
     on_attach = on_attach,
-}
-require('lspconfig')['terraformls'].setup{
+  }
+  require('lspconfig')['terraformls'].setup{
     on_attach = on_attach,
-}
-require('lspconfig')['ts_ls'].setup{
+  }
+  require('lspconfig')['ts_ls'].setup{
     on_attach = on_attach,
-}
-require('lspconfig')['vimls'].setup {
+  }
+  require('lspconfig')['vimls'].setup {
     on_attach = on_attach,
-}
+  }
+
 end
